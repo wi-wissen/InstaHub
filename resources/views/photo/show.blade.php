c176e200c67508bb61f631d50ba4ae4e854b0e7a
Julian Dorn <post@wi-wissen.de>
2018-07-04 15:24:10 +0200

    small html fix

--- resources/views/photo/show.blade.php
+++ resources/views/photo/show.blade.php
@@ -6,10 +6,8 @@
 	@include('flash::message')
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
 		    @include('photo.photo', ['photo' => $photo, 'single' => true])
       </div>
     </div>
-	</div>
 </div>
 @endsection
