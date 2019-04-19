65fdfc26a3cc85fed3a8e53e26f42c934fff1fc3
Julian Dorn <post@wi-wissen.de>
2019-04-19 11:55:07 +0200

    upgrade to laravel 5.8

---
+++ webpack.mix.js
@@ -0,0 +1,24 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js').extract(['vue', 'popper.js', 'axios', 'lodash', 'bootstrap', 'codemirror'])
+   .sass('resources/sass/app.scss', 'public/css')
+   .disableSuccessNotifications();
+
+if (mix.inProduction()) {
+    mix.version();
+}
+
+if (!mix.inProduction()) {
+    mix.sourceMaps();
+}
