e3040f6af3677063c4661df4411ed48426a3bd25
Julian Dorn <post@wi-wissen.de>
2019-04-23 20:36:48 +0200

    web.php does job of a api.php at the moment
    reason: crfc

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,10 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+*/
