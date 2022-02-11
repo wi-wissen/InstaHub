ab15f279f747c120d0dfe57adaa27774b1969e79
Shift <shift@laravelshift.com>
2022-02-11 23:23:14 +0000

    Remove unnecessary `$model` property

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,13 +18,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class UserFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = \App\Models\User::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
@@ -34,8 +27,8 @@ class UserFactory extends Factory
         static $password;
 
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->safeEmail,
+            'name' => $this->faker->name(),
+            'email' => $this->faker->safeEmail(),
             'password' => $password ?: $password = bcrypt('secret'),
             'remember_token' => str_random(10),
         ];
