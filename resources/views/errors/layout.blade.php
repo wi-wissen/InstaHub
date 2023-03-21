f409854a7e8737c36e8ef0bc22efab8ab324f7c6
Julian Dorn <post@wi-wissen.de>
2023-03-21 16:04:13 +0100

    Use local fonts and remove Google fonts import
    @TobiGr points out that we have to remove Google fonts on error pages: https://github.com/wi-wissen/InstaHub/pull/67/files

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -7,15 +7,12 @@
 
         <title>@yield('title')</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
         <!-- Styles -->
         <style>
             html, body {
                 background-color: #fff;
                 color: #636b6f;
-                font-family: 'Raleway', sans-serif;
+                font-family: "Source Sans Pro", "Helvetica Neue", Helvetica, Arial, sans-serif;
                 font-weight: 100;
                 height: 100vh;
                 margin: 0;
