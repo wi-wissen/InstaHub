5e48d8bca6d5474f78ed01a7325865d47cf72992
Julian Dorn <post@wi-wissen.de>
2023-04-07 19:20:21 +0200

    improve ad displaying logic
    support simple - check for id
    variables - replace $user and $photo
    case - support query which result single case

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -22,43 +22,74 @@ class Ad extends Model
         }
 
         foreach ($ads as $ad) {
+            $checkIfResultExist = false;
+
             //prepare query
             $sql = $ad->query;
-            $sql = str_replace('$user', Auth::id(), $sql);
-            $sql = str_replace('$photo', $photo_id, $sql);
+            if (strpos($sql, '$user') !== false) {
+                $checkIfResultExist = true;
+                $sql = str_replace('$user', Auth::id(), $sql);
+            }
+            if (strpos($sql, '$photo') !== false) {
+                $checkIfResultExist = true;
+                $sql = str_replace('$photo', $photo_id, $sql);
+            }
 
             //run query
             try {
                 $r = DB::select($sql);
                 $r = (array) $r;
             } catch (\Illuminate\Database\QueryException $ex) {
-                $r = null;
+                $r = [];
                 flash('<p> Ad <b>'.$ad->name.'</b> is broken: </p><p>'.$ex->getMessage().'</p>', 'danger')->important();
             }
 
-            if ($r) { //check if someone wants to indicate with an empty result that this is false
-                $r = (array) $r;
-                if (count($r) > 1) {
-                    //list of possible ids
-                    for ($i = 0; $i < count($r); $i++) {
-                        if ((current($r[$i]) == Auth::id() && ! $photo_id) ||
-                            (current($r[$i]) == $photo_id && $photo_id)) {
-                            $id = $ad->id;
+            //check query result
+            if(count($r)) {
+                if($checkIfResultExist) {
+
+                    //check if `CASE` was used
+                    if (count($r) == 1 && strpos(strtolower(key((array)$r[0])), 'case') === 0) {
+                        $result = (bool) $r[0]->{key((array)$r[0])};
+                        if($result) {
+                            return self::find($ad->id);
                         }
                     }
-                    if ($id) {
-                        break;
-                    } //leaves foreach
-                } else {
-                    //true or false
-                    if (current($r[0])) {
-                        $id = $ad->id;
-                        break; //leaves foreach
+                    //or just return true if one or more results are given
+                    else {
+                        return self::find($ad->id);
+                    }
+                }
+                else {
+                    //find matching column
+                    $columnValue = $photo_id ?? Auth::id(); //use photo id if set or user id
+                    $columnName = null;
+                    if($photo_id && property_exists($r[0], 'photo_id')) {
+                        $columnValue = $photo_id;
+                        $columnName = 'photo_id';
+                    }
+                    else if(property_exists($r[0], 'user_id')) {
+                        $columnValue = Auth::id();
+                        $columnName = 'user_id';
+                    }
+                    else if(property_exists($r[0], 'id')) {
+                        $columnName = 'id';
+                    }
+
+                    if($columnName) {
+                        foreach ($r as $row) {
+                            if($row->{$columnName} == $columnValue) {
+                                return self::find($ad->id);
+                            }
+                        }
+                    }
+                    else {
+                        flash('<p> Ad <b>'.$ad->name.'</b> is broken: </p><p>No known Column found.</p>', 'danger')->important();
                     }
                 }
             }
         }
 
-        return self::find($id);
+        return null; //return nothing if no matching ad was found
     }
 }
