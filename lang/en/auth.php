56d7fd046db7d5254e28410e301e0b18ba9bf862
Shift <shift@laravelshift.com>
2026-02-10 11:13:05 +0000

    Slim `lang` files

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 return [
     'failed' => 'These credentials do not match our records or is not activated.',
     'password' => 'The password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
 ];
--- lang/en/pagination.php
+++ lang/en/pagination.php
@@ -3,6 +3,5 @@
 declare(strict_types=1);
 
 return [
-    'next' => 'Next &raquo;',
-    'previous' => '&laquo; Previous',
-];
+
+    ];
